ae7e5c97f99e04be00b802ac88f4996c590e594b
lambo <lanbohu@myvuw.ac.nz>
2023-11-01 13:34:55 +1300

    Lambo

--- src/application/Main.java
+++ src/application/Main.java
@@ -79,7 +79,6 @@ public class Main extends Application {
 		hBox121.getChildren().addAll(text2);
 		hBox122.getChildren().addAll(passwordField);
 		text2.setText("Password  ");
-		//123
 		passwordField.setPromptText("Password");
 
 		// Login button
